1349641229e75a3fb4c1347584f6ceb82c9ee91a
yu <overthenightmare@gmail.com>
2019-09-05 07:26:22 +0000

    add generate_uid.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var users = require('./routes/users');
+var generate_uid = require('./generate_uid');
 
 var app = express();
 
@@ -11,6 +12,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
-app.use('/api/v1/users', users);
+app.use('/api/v1/generate_uid', generate_uid);
+
+app.listen(8000);
 
 module.exports = app;
