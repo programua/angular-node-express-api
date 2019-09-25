cb54b5d42f0ff4119671b5bf3cc12b4d4481de34
yu <overthenightmare@gmail.com>
2019-09-25 08:51:11 +0000

    (:-D)

--- app.js
+++ app.js
@@ -8,14 +8,23 @@ var customer = require('./routes/customer');
 var generate_uid = require('./generate_uid');
 
 var app = express();
+var options = {
+  setHeaders: function(res, path, stat) {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  }
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
-app.use(function(req, res, next){
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow=Headers", "Origin, X-Requested-With, Content-Type, Accept");
-});
+app.use(express.static('public', options));
+// app.use(function(req, res, next){
+//   res.header('Access-Control-Allow-Origin', '*');
+  // res.setHeader("Access-Control-Allow-Methods","GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  // res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// });
 
 app.use('/api/v1/customer', customer);
 app.use('/api/v1/generate_uid', generate_uid);
--- generate_uid.js
+++ generate_uid.js
@@ -4,6 +4,7 @@ var uid = require("uid-safe");
 var router = express.Router();
 
 router.get('/', function(req, res, next){
+  console.log("generate_uid.js");
   var strUid = uid.sync(18);
   res.json({guid: strUid});
 });
